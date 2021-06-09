c1fe9f22bf143fc2726a44b05b7117dcbb5ec665
Dmitrii Mukhutdinov <flyingleafe@gmail.com>
2021-06-09 16:55:52 +0200

    added list of admin ips to the server start script

--- server-start.sh
+++ server-start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# read admin ips
+ADMIN_IPS_OPTIONS="-a 127.0.0.1"
+while read ip; do
+	ADMIN_IPS_OPTIONS="$ADMIN_IPS_OPTIONS -a $ip"
+done < $SCRIPT_DIR/admin_ips.txt
+
 source $SCRIPT_DIR/bin/activate
-pymushra server
+pymushra server $ADMIN_IPS_OPTIONS
