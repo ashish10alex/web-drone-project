0680e04f50495d53c445d04b3a0beb124ca9e2fe
Ashish Alex <ashish.alex10@gmail.com>
2021-06-15 18:23:48 +0100

    new yaml files

--- generate_all_csv.sh
+++ generate_all_csv.sh
@@ -3,10 +3,10 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # CSV paths generation/folder structure
-python meta_data.py --root_dir $SCRIPT_DIR common
+python3 meta_data.py --root_dir $SCRIPT_DIR common
 
 # Baseline vs. noisy
-python meta_data.py --root_dir $SCRIPT_DIR baseline_noisy
+python3 meta_data.py --root_dir $SCRIPT_DIR baseline_noisy
 
 # All other models
-python meta_data.py --root_dir $SCRIPT_DIR others
+python3 meta_data.py --root_dir $SCRIPT_DIR others
